7e92b0c7767c93cd02bef76cd7bfa11e9195ff13
Sayid Adam <mryunkaka@gmail.com>
2026-02-23 13:53:26 +0800

    Update Error

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+// Suppress PHP 8+ deprecated warnings for CodeIgniter 3 compatibility
+error_reporting(E_ERROR | E_PARSE);
+ini_set('display_errors', 0);
 
 /**
  * CodeIgniter
@@ -54,7 +57,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 /*
  *---------------------------------------------------------------
@@ -67,21 +70,14 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
+		error_reporting(E_ERROR | E_PARSE);
+		ini_set('display_errors', 0);
 	break;
 
 	case 'testing':
 	case 'production':
 		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
+		error_reporting(E_ERROR | E_PARSE);
 	break;
 
 	default:
